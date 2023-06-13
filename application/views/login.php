0ad215f4caccaee466a8e1a320c18aa5c2ff9602
hurimen <135969873+hurimen@users.noreply.github.com>
2023-06-13 08:14:33 +0700

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -5,7 +5,8 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Si Ormawa</title>
+<title>SIMPLORMAWA</title>
+        <link rel="icon" href="<?=base_url()?>assets/unipdu.ico" type="image/x-icon">
 <!-- Bootstrap CSS -->
 <link href="<?php echo base_url() ?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="<?php echo base_url() ?>assets/css/style.css" rel="stylesheet" type="text/css" />
